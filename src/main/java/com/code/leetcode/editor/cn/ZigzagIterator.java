f799bee88708c1b713239ca7ed2503363507caf9
luckywzy <18792470504@163.com>
2021-11-10 20:52:38 +0800

    ZigzagIteratorK 问题解决

--- src/main/java/com/code/leetcode/editor/cn/ZigzagIterator.java
+++ src/main/java/com/code/leetcode/editor/cn/ZigzagIterator.java
@@ -69,6 +69,62 @@ public class ZigzagIterator {
     int pos2 = 0;
     List<Integer> v1;
     List<Integer> v2;
+
+    /**
+     * 进阶：
+     * K个数组，klist
+     * 使用pos[k-1] 来表示 第K 个数组遍历到的位置
+     * 使用 point 指向 当前遍历到的数组
+     * 使用 total 表示数据总长度
+     * hasNext 需要汇总一下 pos[0,,,k-1]的和 与total 的大小
+     * next 需要判断当前point指向的数组是否未遍历完，
+     *      否，point 取下一个数组，
+     *      是则取当前的 kList.get(point).get(posTablek[point])， 同时自增 posTablek[point]， point
+     *      注：point >= k 时 需要 将 point 重置为0
+     */
+    public static class ZigzagIteratorK {
+        List<List<Integer>> kList;
+
+        public ZigzagIteratorK(List<List<Integer>> kList) {
+            this.kList = kList;
+            posTablek = new int[kList.size()];
+            for (List<Integer> list : kList) {
+                total += list.size();
+            }
+        }
+
+        public boolean hasNext() {
+            int p = 0;
+            for (int pos : posTablek) {
+                p += pos;
+            }
+            return p < total;
+        }
+
+        public int next() {
+            int point1 = point;
+            //posTablek[point1] 当前指向的数组指向的pos
+            while (posTablek[point1] >= kList.get(point1).size()) {
+                point1++;
+                if (point1 >= posTablek.length) {
+                    point1 = 0;
+                }
+            }
+            Integer val = kList.get(point1).get(posTablek[point1]);
+            posTablek[point1]++;
+            point++;
+            if (point >= posTablek.length) {
+                point = 0;
+            }
+            return val;
+        }
+
+        //list指向的pos
+        int[] posTablek;
+        //当前指向的list
+        int point;
+        int total;
+    }
 }
 
 /**
--- src/test/java/com/code/leetcode/editor/cn/ZigzagIteratorTest.java
+++ src/test/java/com/code/leetcode/editor/cn/ZigzagIteratorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author wangzongyu
@@ -46,4 +47,38 @@ public class ZigzagIteratorTest {
             System.out.print(zigzagIterator.next() + ",");
         }
     }
+
+    @Test
+    public void nextK_1() {
+        Integer[] v1 = new Integer[]{1};
+        Integer[] v2 = new Integer[]{5};
+        ArrayList<List<Integer>> kList = new ArrayList<>();
+        kList.add(Arrays.asList(v1));
+        kList.add(Arrays.asList(v2));
+        ZigzagIterator.ZigzagIteratorK zigzagIteratorK = new ZigzagIterator.ZigzagIteratorK(kList);
+        //  [1,3,2,4,5,6]
+        while (zigzagIteratorK.hasNext()) {
+            System.out.print(zigzagIteratorK.next() + ",");
+        }
+    }
+
+
+    @Test
+    public void nextK_2() {
+        Integer[] v1 = new Integer[]{1, 2, 3};
+        Integer[] v2 = new Integer[]{4, 5, 6, 7};
+        Integer[] v3 = new Integer[]{8, 9};
+        ArrayList<List<Integer>> kList = new ArrayList<>();
+        kList.add(Arrays.asList(v1));
+        kList.add(Arrays.asList(v2));
+        kList.add(Arrays.asList(v3));
+        ZigzagIterator.ZigzagIteratorK zigzagIteratorK = new ZigzagIterator.ZigzagIteratorK(kList);
+        //  [1,3,2,4,5,6]
+        System.out.println("1,4,8,2,5,9,3,6,7");
+        while (zigzagIteratorK.hasNext()) {
+            System.out.print(zigzagIteratorK.next() + ",");
+        }
+    }
+
+
 }
